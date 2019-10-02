6d61ec9d4c8b2b39e807f8ff5086d12856d6f2a2
Gunharth Randolf <hello@gunharth.io>
2019-10-02 22:22:31 +0200

    Amend notification message

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener('push', function (event) {
   const payload = event.data ? event.data.text() : 'no payload';
   event.waitUntil(
-    self.registration.showNotification('ServiceWorker Cookbook', {
+    self.registration.showNotification('Serverless Lambda Pusher', {
       body: payload,
     })
   );
